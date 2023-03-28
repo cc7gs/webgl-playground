0facc1254657e9d0198f83b59374745c24f35dc0
cc7gs <chenorange12@gmail.com>
2023-03-28 18:41:40 +0800

    chore: 透视投影

---
+++ docs/demos/triangle/perspectiveViewMVP.tsx
@@ -0,0 +1,126 @@
+import { mat4 } from 'gl-matrix';
+import Draw from '../Draw';
+import { initShaderProgram } from '../utils';
+
+export default function Basic() {
+  return <Draw main={main} />;
+}
+
+function main(canvas: HTMLCanvasElement) {
+  const gl = canvas.getContext('webgl');
+  if (!gl) {
+    alert('无法初始化 WebGL，你的浏览器、操作系统或硬件等可能不支持 WebGL。');
+    return;
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // 顶点着色器片段
+  const vsSource = `
+      attribute vec4 aVertexPosition;
+      attribute vec4 aVertexColor;
+      uniform mat4 uModelMatrix;
+      uniform mat4 uViewMatrix;
+      uniform mat4 uPerspectiveMatrix;
+      varying  vec4 vColor;
+      void main(){
+          gl_Position = uPerspectiveMatrix * uViewMatrix * uModelMatrix * aVertexPosition;
+          vColor = aVertexColor;
+      }
+  `;
+  // 片元着色器片段
+  const fsSource = `
+  varying lowp vec4 vColor;
+  void main(){
+      gl_FragColor = vColor;
+  }
+  `;
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  if (!shaderProgram) throw new Error('initShaderProgram failed');
+  const n = initVertexBuffer(gl, shaderProgram);
+
+  // 获取 uniform 变量的存储位置 
+  const uModelMatrix = gl.getUniformLocation(shaderProgram, 'uModelMatrix');
+  const uViewMatrix = gl.getUniformLocation(
+    shaderProgram,
+    'uViewMatrix',
+  );
+  const uPerspectiveMatrix = gl.getUniformLocation(shaderProgram, 'uPerspectiveMatrix');
+
+  // 模型矩阵
+  const modelRightMatrix = mat4.create();
+  // 视图矩阵
+  const viewMatrix = mat4.create();
+  // 透视投影矩阵
+  const perspectiveMatrix = mat4.create();
+
+  // 计算模型矩阵、视图矩阵、透视投影矩阵
+  mat4.translate(modelRightMatrix, modelRightMatrix, [0.75, 0, 0]);
+  mat4.lookAt(viewMatrix, [0, 0, 5], [0, 0, -100], [0, 1, 0]);
+  mat4.perspective(perspectiveMatrix, Math.PI / 6, canvas.width / canvas.height, 1, 100);
+
+  gl.uniformMatrix4fv(uModelMatrix, false, modelRightMatrix);
+  // 将视图矩阵传输给顶点着色器
+  gl.uniformMatrix4fv(uViewMatrix, false, viewMatrix);
+  // 将透视投影矩阵传输给顶点着色器
+  gl.uniformMatrix4fv(uPerspectiveMatrix, false, perspectiveMatrix);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+  
+  // 模型矩阵
+  const modelLeftMatrix = mat4.create();
+  // 左侧三角形
+  mat4.translate(modelLeftMatrix, modelLeftMatrix, [-0.75, 0, 0]);
+  // 修改模型矩阵
+  gl.uniformMatrix4fv(uModelMatrix, false, modelLeftMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function initVertexBuffer(gl: WebGLRenderingContext, program: WebGLProgram) {
+  // prettier-ignore
+  const verticesColors = new Float32Array([
+    // 顶点坐标和颜色
+    0.0, 1.0, -4.0, 0.4, 1.0, 0.4,1.0, // 绿色三角形在最后面
+    -0.5, -1.0, -4.0, 0.4, 1.0, 0.4,1.0,
+    0.5, -1.0, -4.0,  0.4, 1.0, 0.4,1.0,
+    // 黄色三角形在中间
+    0.0, 1.0, -2.0, 1.0, 0.4, 0.4,1.0,
+    -0.5, -1.0, -2.0, 1.0, 1.0, 0.4,1.0,
+    0.5, -1.0, -2.0, 1.0, 1.0, 0.4,1.0,
+    // 蓝色三角形在最前面
+    0.0, 1.0, 0.0, 0.4, 0.4, 1.0,1.0,
+    -0.5, -1.0, 0.0, 0.4, 0.4, 1.0,1.0,
+    0.5, -1.0, 0.0, 1.0, 0.4, 0.4,1.0,
+  ]);
+  // 顶点个数
+  const n = 9;
+
+  // 创建缓冲区对象
+  const vertexColorBuffer = gl.createBuffer();
+  if (!vertexColorBuffer) {
+    alert('Failed to create the buffer object');
+    return -1;
+  }
+
+  // 将缓冲区对象绑定到目标
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  // 向缓冲区对象中写入数据
+  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+  const FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
+  // 获取attribute变量的存储位置
+  const aPosition = gl.getAttribLocation(program, 'aVertexPosition');
+  // 将缓冲区对象分配给aPosition变量
+  gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, FSIZE * 7, 0);
+  // 连接aPosition变量与分配给它的缓冲区对象
+  gl.enableVertexAttribArray(aPosition);
+
+  const aColor = gl.getAttribLocation(program, 'aVertexColor');
+  gl.vertexAttribPointer(aColor, 4, gl.FLOAT, false, FSIZE * 7, FSIZE * 3);
+  gl.enableVertexAttribArray(aColor);
+
+  return n;
+}
---
+++ docs/playground/games101.md
@@ -0,0 +1,10 @@
+---
+title: 图形学相关知识
+group:
+  title: 前置知识
+order: 1
+---
+
+- https://www.yuque.com/mashimaro-qhlzj/wccmw/uq8fww4u7y2wv7lg?singleDoc# 《【WIP】GAMES101- 图形学笔记》
+
+- https://www.yuque.com/mashimaro-qhlzj/wccmw/in7nditicc3ba7nz?singleDoc# 《【WIP】webGL》
